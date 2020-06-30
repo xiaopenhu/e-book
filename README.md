c48125baf303d3722e08ac03017d5d18adb1fcd3
王雨 <17637000021@163.com>
2020-06-30 17:49:15 +0800

    'icon-demo'

--- README.md
+++ README.md
@@ -2,3 +2,9 @@
 
 ## vue-cli4安装
 报考vuex、router、配置选项保存在各自文件、已经基础配置（babel + ESlint）
+
+## 字体图标
+1. 去阿里图标库下载svg图标
+2. https://icomoon.io/app/#/select  ，点击左上角，会有New Empty Set 选项
+3. 之后修改项目名，导入字体图标，全选后，下方第三个选项下载
+4. 把style.css和font文件夹引入assets即可
--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,43 +1,23 @@
 <template>
   <div class="hello">
-    <div id="read"></div>
+    <div id="read">
+      <span class="icon-bookmark"></span>
+      <span class="icon-clock"></span>
+    </div>
   </div>
 </template>
 
 <script>
-import Epub from 'epubjs'
 
-global.ePub = Epub
 export default {
-  name: 'HelloWorld',
-  mounted () {
-    this.book = new Epub('/2016.epub')
-    this.book.renderTo('read', {
-      width: window.innerWidth,
-      height: window.innerHeight
-    }).display()
-    console.log(this.book)
-  }
+  name: 'HelloWorld'
 }
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-  h3 {
-    margin: 40px 0 0;
-  }
-
-  ul {
-    list-style-type: none;
-    padding: 0;
-  }
-
-  li {
-    display: inline-block;
-    margin: 0 10px;
-  }
-
-  a {
-    color: #42b983;
-  }
+.icon-bookmark {
+  font-size: 40px;
+  color: red;
+}
 </style>
